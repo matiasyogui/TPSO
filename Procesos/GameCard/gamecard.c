2b9f6aa297c15bdec47700d97bbb4aa1b29ea7c2
willian <willianramirez33@gmail.com>
2020-06-09 19:47:39 -0300

    hola

--- Procesos/GameCard/gamecard.c
+++ Procesos/GameCard/gamecard.c
@@ -1,6 +1,12 @@
+#include <stdio.h>
+#include <sys/mount.h>
 
+const char* path = "/home/utnso/workspace/tp-2020-1c-Bomberman-2.0/Procesos/GameCard/TallGrass";
 
 int main(){
 	
+	if( mount(path, "/sys", "sysfs", 0, "") != 0)
+		printf("error al montar");
+
 	return 0;
 }
