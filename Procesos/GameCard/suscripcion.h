43942486cb00abff7c0dd0cd90eafd18fb801dda
daniel lunati <dlunati.ko85@gmail.com>
2020-06-29 01:56:53 -0300

    lectura estructura file en get_pokemon

--- Procesos/GameCard/suscripcion.h
+++ Procesos/GameCard/suscripcion.h
@@ -1,9 +1,25 @@
 #ifndef SUSCRIPCION_H_
 #define SUSCRIPCION_H_
 
+#include <commons/collections/list.h>
 #include <pthread.h>
+#include <signal.h>
+#include <cosas_comunes.h>
 #include "variables_globales.h"
 
+
+typedef struct{
+	int id;
+	int cod_op;
+	t_buffer* buffer;
+}t_mensajeTeam;
+
+
+int mensajeActual;
+
+void iniciar_servidor(void);
+char* leer_get_pokemon(int);
+char* leer_mensaje_getPokemon(t_buffer*);
 void iniciar_suscripciones(int cola0, int cola1, int cola2);
 void finalizar_suscripciones(void);
 
