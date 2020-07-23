41e8b124d4d6856aadeed5f4257cd4b126f0d023
Matias Yogui <matiasyogui@hotmail.com>
2020-07-23 18:41:17 -0300

    lockeo de archivos

---
+++ Procesos/GameCard/lock.c
@@ -0,0 +1,14 @@
+#include "lock.h"
+
+void abrirArchivoSinoEspero(char* ruta){
+
+	t_config* metadata = config_create(ruta);
+
+	char* abierto = config_get_string_value(metadata, "OPEN");
+
+	while(string_equals_ignore_case(abierto, "Y")){
+		sleep(TIEMPO_DE_REINTENTO_OPERACION);
+	}
+
+	config_set_value(metadata, "OPEN", "Y");
+}
